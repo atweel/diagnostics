c2cb61e0f43ce28dfd93e5789f69689ee04baaaf
Eduard Malakhov <contactme@eduardmalakhov.com>
2020-06-27 21:26:51 +0700

    chore: refactor jest configuration with projects

--- jest.config.js
+++ jest.config.js
@@ -1,22 +1,8 @@
-const { pathsToModuleNameMapper } = require('ts-jest/utils');
-// In the following statement, replace `./tsconfig` with the path to your `tsconfig` file
-// which contains the path mapping (ie the `compilerOptions.paths` option):
-const path = require('path');
-
-const tsConfigPath = path.join(process.cwd(), 'tsconfig');
-
-const { compilerOptions } = require(tsConfigPath);
-
 module.exports = {
     // globalSetup: './tests/hooks/setup.js',
     // globalTeardown: './tests/hooks/teardown.js',
-    preset: 'ts-jest',
-    moduleNameMapper: pathsToModuleNameMapper(compilerOptions.paths, {
-        prefix: path.resolve(compilerOptions.baseUrl) + path.sep,
-    }),
-    globals: {
-        'ts-jest': {    
-            tsConfig: tsConfigPath + '.json'
-        }
-    }
+    "projects": [
+        "packages/core",
+        "packages/extensions/console",
+    ]
 };
